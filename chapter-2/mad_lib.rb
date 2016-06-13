9df2281b325d6b2bfabaf2cbd020ef1defcd56fd
Arthur Moura Carvalho <armoucar@gmail.com>
2016-06-13 14:11:17 -0300

    Chapter 2, Exercise 4

---
+++ chapter-2/mad_lib.rb
@@ -0,0 +1,20 @@
+# Constraints:
+#
+# * Use a single output statement for this program.
+#
+# * If your language supports string interpolation or string substitution, use it to build up the output.
+
+puts "Enter a noun:"
+noun = gets.chomp
+
+puts "Enter a verb:"
+verb = gets.chomp
+
+puts "Enter a adjective:"
+adjective = gets.chomp
+
+puts "Enter a adverb:"
+adverb = gets.chomp
+
+puts "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious"
+
