b736899860d137675075712f088a90096209e736
Arthur Moura Carvalho <armoucar@gmail.com>
2016-06-13 14:44:27 -0300

    Exercise challenges: accept only valid numbers; don't accept negative numbers; break the program into functions

--- chapter-2/simple_math.rb
+++ chapter-2/simple_math.rb
@@ -4,11 +4,39 @@
 # * Keeptheinputsandoutputsseparatefromthenumerical conversions and other processing.
 # * Generate a single output statement with line breaks in the appropriate spots.
 
-puts "What's the first number?"
-first = gets.chomp
+def is_number?(number)
+  !!Float(number)
+rescue ArgumentError
+  false
+end
 
-puts "What's the second number?"
-second = gets.chomp
+def validate(condition, message)
+  if !condition
+    puts message
+    return false
+  end
+
+  true
+end
+
+def get_number_from_user_input(prompt)
+  number = 0
+
+  loop do
+    puts prompt
+    number = gets.chomp
+
+    next if !validate(is_number?(number), "You should enter a valid number.")
+    next if !validate(number.to_f >= 0, "You should enter a positive number.")
+
+    break
+  end
+
+  number
+end
+
+first = get_number_from_user_input "What's the first number?"
+second = get_number_from_user_input "What's the second number?"
 
 puts %Q(#{first} + #{second} = #{first.to_f + second.to_f}
 #{first} - #{second} = #{first.to_f - second.to_f}
