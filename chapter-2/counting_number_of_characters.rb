029fdb0b1795983c096859bdd76c40dd98f1c7b7
Arthur Moura Carvalho <armoucar@gmail.com>
2016-06-13 13:56:21 -0300

    State that the user must enter something into the program

--- chapter-2/counting_number_of_characters.rb
+++ chapter-2/counting_number_of_characters.rb
@@ -1,3 +1,10 @@
-puts "What's the input string?"
-input_text = gets.chomp
+input_text = nil
+
+loop do
+  puts "What's the input string?"
+  input_text = gets.chomp
+
+  break if input_text.length > 0
+end
+
 puts "'#{input_text}' has #{input_text.length} characters."
